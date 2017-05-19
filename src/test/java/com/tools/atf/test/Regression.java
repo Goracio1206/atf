f9dd51643a8159957d9c2ec1b5dac1bc8db819aa
Admin <vitalii.nedzelskyi@gmail.com>
2017-05-19 18:05:16 +0300

    Added new dedpendency for Hamcrest assertions

--- src/test/java/com/tools/atf/test/Regression.java
+++ src/test/java/com/tools/atf/test/Regression.java
@@ -7,17 +7,14 @@ import Web.WebPages.MainMenu;
 import Web.WebPages.MainPage;
 import org.testng.annotations.Test;
 
-public class FirstTest extends BaseTest {
+public class Regression extends BaseTest {
 
     @Test
-    public void TestMethod() {
+    public void _SimpleLoginAndLogOut() {
         new MainPage(driver).loadAnsWaitUntilAvailable().goToLoginPage();
         new LoginPage(driver).waitUntilAvailable().loginAs("test", "test");
         new AdminPage(driver).waitUntilAvailable();
         new MainMenu(driver).waitUntilAvailable().logOut();
-        try {
-            Thread.sleep(5000);
-        } catch (Exception e) {
-        }
+
     }
 }
