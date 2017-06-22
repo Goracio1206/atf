c886f7b5af42741224b7d4c278555c70091bde2f
Admin <vitalii.nedzelskyi@gmail.com>
2017-06-23 00:56:03 +0300

    fix

--- src/main/java/Web/WebPages/Categories.java
+++ src/main/java/Web/WebPages/Categories.java
@@ -1,35 +1,47 @@
 package Web.WebPages;
 
 import Web.WebComponent;
-import Web.WebElements.List;
+import Web.WebElements.ComponentList;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import java.util.ArrayList;
+import org.openqa.selenium.WebElement;
 
-import static Core.TestData.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Admin on 19-Jun-17.
  */
 public class Categories extends WebComponent<Categories> {
 
-    List list;
+    public List<WebElement> categoriesItems;
+    public Map<String, Integer> topicPerCategory = new HashMap<String, Integer>();
 
     public Categories(WebDriver driver, By findByMethod) {
         super(driver, findByMethod);
-        list = new List(driver, By.xpath(CATEGORIES_SECTION));
+        categoriesItems = new ComponentList(driver, findByMethod).getComponentList();
+        this.setTopicPerCategory();
     }
 
-    public boolean isPresent(String categoryName) {
-        ArrayList<String> allCategory = list.getList();
-        for (String elem : allCategory) {
-            System.out.println(elem);
-        }
-        for (String elem : allCategory) {
-            if (elem.contains(categoryName)) {
+    public boolean isNewCategoryPresent(String categoryName) {
+        for (Map.Entry<String, Integer> elem : topicPerCategory.entrySet()) {
+            if (elem.getKey().equals(categoryName) && elem.getValue().equals(0)) {
                 return true;
             }
         }
         return false;
+    }
+
+    private void setTopicPerCategory() {
+        String categoryName;
+        Integer topicsNumber;
+        for (WebElement element : categoriesItems) {
+            String[] mass = new String[3];
+            mass = element.getText().split("[()]");
+            categoryName = mass[0];
+            topicsNumber = Integer.valueOf(mass[1]);
+            topicPerCategory.put(mass[0], Integer.valueOf(mass[1]));
+        }
     }
 }
